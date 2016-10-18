8d4780826c8a37250843071418287443f0ae3d91
Artem <arzek2015@gmail.com>
2016-10-18 20:57:10 +0300

    Label

--- backend/models/Group.php
+++ backend/models/Group.php
@@ -42,8 +42,8 @@ class Group extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
-            'name' => 'Name',
-            'date' => 'Date',
+            'name' => 'Имя',
+            'date' => 'Дата создания',
         ];
     }
 
