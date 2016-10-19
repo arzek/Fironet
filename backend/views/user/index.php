805f768d57102c324e298e9629a626a10f67f5a8
Artem <arzek2015@gmail.com>
2016-10-19 10:26:02 +0300

    Index page

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -7,7 +7,7 @@ use yii\grid\GridView;
 /* @var $searchModel app\models\UserSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Users';
+$this->title = 'Пользователи';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-index">
@@ -16,7 +16,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
     <p>
-        <?= Html::a('Create User', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Создать пользователя', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
@@ -24,10 +24,22 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-            'id',
+            //'id',
             'name',
-            'id_group',
-            'date',
+            ['attribute' => 'id_group',
+                'format' => 'text',
+                'value'=>function($data)
+                {
+                    return $data->getGroup();
+                },
+            ],
+            ['attribute' => 'date',
+                'format' => 'text',
+                'value'=>function($data)
+                {
+                    return $data->getDate();
+                },
+            ],
 
             ['class' => 'yii\grid\ActionColumn'],
         ],
