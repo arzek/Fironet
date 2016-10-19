468acf80e4d3fbadf61e81abcc29a5447f97f192
Artem <arzek2015@gmail.com>
2016-10-19 14:39:25 +0300

    Start group api

--- api/models/Group.php
+++ api/models/Group.php
@@ -22,4 +22,25 @@ class Group extends \yii\db\ActiveRecord
     {
         return 'group';
     }
+
+
+    public function fields()
+    {
+        return [
+
+            'id',
+
+            'name',
+
+
+            'date' => function(){
+                return $this->getDate();
+            }
+        ];
+    }
+
+    public function getDate()
+    {
+        return date('  H:i:s d-m-Y', strtotime($this->date));
+    }
 }
